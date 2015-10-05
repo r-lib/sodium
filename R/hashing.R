b99cfae80aef9432ba5f735cc5e87a8a06558469
Jeroen Ooms <jeroenooms@gmail.com>
2015-10-05 15:05:01 +0200

    More shorthash

--- R/hashing.R
+++ R/hashing.R
@@ -1,33 +1,55 @@
 #' Hash functions
 #'
-#' Functions to calculate a fixed length hash (digest) of a message. Can also be used
-#' as HMAC by specifying a (sercret) \code{key}. In this case length of the \code{key}
-#' has to match the length of the output of the hash function.
+#' Functions to calculate cryptographic hash of a message, with optionally a key for
+#' HMAC applications
+#'
+#' The generic \code{hash} function is recommended. It uses dynamic length
+#' \href{https://download.libsodium.org/doc/hashing/generic_hashing.html}{BLAKE2b}
+#' where output size can be any value between 16 bytes (128bit) and 64 bytes (512bit).
+#' We can use each function as HMAC by specifying a \code{key}. They key size for
+#' \code{shorthash} is 16 bytes, for \code{sha256} it is 32 bytes and for \code{sha512}
+#' it is 64 bytes. For \code{hash} the key size can be any value between 16 and 62,
+#' recommended is at least 32.
+#'
+#' The \code{shorthash} function is a special 8 byte (64 bit) hash based on
+#' \href{https://download.libsodium.org/doc/hashing/short-input_hashing.html}{SipHash-2-4}.
+#' The output of this function is only 64 bits (8 bytes). It is useful for in e.g.
+#' Hash tables, but it should not be considered collision-resistant.
 #'
 #' @rdname hash
 #' @name hash
-#' @useDynLib sodium R_sha256 R_auth_sha256
+#' @references \url{https://download.libsodium.org/doc/hashing/generic_hashing.html}
+#' @useDynLib sodium R_crypto_generichash
 #' @param buf raw vector with data to be hashed
-#' @param key optional. raw vector with key to be used for HMAC hashing
+#' @param key raw vector with key for HMAC hashing. Optional, except for in \code{shorthash}.
 #' @export
 #' @examples # Basic hashing
 #' msg <- serialize(iris, NULL)
+#' hash(msg)
 #' sha256(msg)
 #' sha512(msg)
 #'
 #' # HMAC hash
-#' key256 <- rand_bytes(32)
-#' key512 <- rand_bytes(64)
-#' sha256(msg, key = key256)
-#' sha512(msg, key = key512)
-sha256 <- function(buf, key = NULL){
+#' key <- rand_bytes(32)
+#' hash(msg, key = key)
+#' sha256(msg, key = key)
+#'
+#' # Short hash
+#' shortkey <- rand_bytes(16)
+#' shorthash(msg, shortkey)
+hash <- function(buf, size = 32, key = NULL){
   stopifnot(is.raw(buf))
-  if(length(key)){
-    stopifnot(is.raw(key))
-    .Call(R_auth_sha256, buf, key)
-  } else {
-    .Call(R_sha256, buf)
-  }
+  stopifnot(is.null(key) || is.raw(key))
+  .Call(R_crypto_generichash, buf, size, key)
+}
+
+#' @export
+#' @rdname hash
+#' @useDynLib sodium R_crypto_shorthash
+shorthash <- function(buf, key){
+  stopifnot(is.raw(buf))
+  stopifnot(is.raw(key))
+  .Call(R_crypto_shorthash, buf, key)
 }
 
 #' @rdname hash
@@ -40,5 +62,19 @@ sha512 <- function(buf, key = NULL){
     .Call(R_auth_sha512, buf, key)
   } else {
     .Call(R_sha512, buf)
+  }
+}
+
+#' @rdname hash
+#' @param size the size of the output hash. Must be between 16 and 64, recommended is 32.
+#' @useDynLib sodium R_sha256 R_auth_sha256
+#' @export
+sha256 <- function(buf, key = NULL){
+  stopifnot(is.raw(buf))
+  if(length(key)){
+    stopifnot(is.raw(key))
+    .Call(R_auth_sha256, buf, key)
+  } else {
+    .Call(R_sha256, buf)
   }
 }
--- src/hashing.c
+++ src/hashing.c
@@ -1,6 +1,8 @@
 #include <Rinternals.h>
 #include <sodium.h>
 
+/* SHA256 */
+
 SEXP R_sha256(SEXP buf){
   SEXP res = allocVector(RAWSXP, crypto_hash_sha256_BYTES);
   if(crypto_hash_sha256(RAW(res), RAW(buf), LENGTH(buf)))
@@ -17,6 +19,8 @@ SEXP R_auth_sha256(SEXP buf, SEXP key){
   return res;
 }
 
+/* SHA512 */
+
 SEXP R_sha512(SEXP buf){
   SEXP res = allocVector(RAWSXP, crypto_hash_sha512_BYTES);
   if(crypto_hash_sha512(RAW(res), RAW(buf), LENGTH(buf)))
@@ -29,6 +33,40 @@ SEXP R_auth_sha512(SEXP buf, SEXP key){
     Rf_error("Invalid key, must be exactly %d bytes", crypto_auth_hmacsha512_BYTES);
   SEXP res = allocVector(RAWSXP, crypto_hash_sha512_BYTES);
   if(crypto_auth_hmacsha512(RAW(res), RAW(buf), LENGTH(buf), RAW(key)))
+    Rf_error("Failed to hash");
+  return res;
+}
+
+/* BLAKE2b */
+
+SEXP R_crypto_generichash(SEXP buf, SEXP size, SEXP key){
+  int outlen = asInteger(size);
+  if(outlen < crypto_generichash_BYTES_MIN || outlen > crypto_generichash_BYTES_MAX)
+    Rf_error("Invalid output length, must be in between %d and %d", crypto_generichash_BYTES_MIN, crypto_generichash_BYTES_MAX);
+
+  unsigned char *keyval = NULL;
+  int keysize = 0;
+  if(key != R_NilValue){
+    keysize = LENGTH(key);
+    keyval = RAW(key);
+    if(keysize < crypto_generichash_KEYBYTES_MIN || keysize > crypto_generichash_KEYBYTES_MAX)
+      Rf_error("Invalid key size, must be between %d and %d bytes", crypto_generichash_KEYBYTES_MIN, crypto_generichash_KEYBYTES_MAX);
+  }
+
+  SEXP res = allocVector(RAWSXP, outlen);
+  if(crypto_generichash(RAW(res), outlen, RAW(buf), LENGTH(buf), keyval, keysize))
+    Rf_error("Failed to hash");
+  return res;
+}
+
+/* Shorthash */
+
+SEXP R_crypto_shorthash(SEXP buf, SEXP key){
+  if(LENGTH(key) != crypto_shorthash_KEYBYTES)
+    Rf_error("Invalid key, must be exactly %d bytes", crypto_shorthash_KEYBYTES);
+
+  SEXP res = allocVector(RAWSXP, crypto_shorthash_BYTES);
+  if(crypto_shorthash(RAW(res), RAW(buf), LENGTH(buf), RAW(key)))
     Rf_error("Failed to hash");
   return res;
 }
