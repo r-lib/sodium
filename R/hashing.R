8efaf709bc4bafd4b2cccad0fda0c57b8a387016
Jeroen Ooms <jeroenooms@gmail.com>
2015-10-05 13:47:28 +0200

    Keyed hashing for HMAC

--- R/hashing.R
+++ R/hashing.R
@@ -1,19 +1,44 @@
 #' Hash functions
 #'
-#' Calculate digests
+#' Functions to calculate a fixed length hash (digest) of a message. Can also be used
+#' as HMAC by specifying a (sercret) \code{key}. In this case length of the \code{key}
+#' has to match the length of the output of the hash function.
 #'
-#' @rdname hashing
-#' @useDynLib sodium R_sha256
+#' @rdname hash
+#' @name hash
+#' @useDynLib sodium R_sha256 R_auth_sha256
+#' @param buf raw vector with data to be hashed
+#' @param key optional. raw vector with key to be used for HMAC hashing
 #' @export
-sha256 <- function(buf){
+#' @examples # Basic hashing
+#' msg <- serialize(iris, NULL)
+#' sha256(msg)
+#' sha512(msg)
+#'
+#' # HMAC hash
+#' key256 <- rand_bytes(32)
+#' key512 <- rand_bytes(64)
+#' sha256(msg, key = key256)
+#' sha512(msg, key = key512)
+sha256 <- function(buf, key = NULL){
   stopifnot(is.raw(buf))
-  .Call(R_sha256, buf)
+  if(length(key)){
+    stopifnot(is.raw(key))
+    .Call(R_auth_sha256, buf, key)
+  } else {
+    .Call(R_sha256, buf)
+  }
 }
 
-#' @rdname hashing
-#' @useDynLib sodium R_sha512
+#' @rdname hash
+#' @useDynLib sodium R_sha512 R_auth_sha512
 #' @export
-sha512 <- function(buf){
+sha512 <- function(buf, key = NULL){
   stopifnot(is.raw(buf))
-  .Call(R_sha512, buf)
+  if(length(key)){
+    stopifnot(is.raw(key))
+    .Call(R_auth_sha512, buf, key)
+  } else {
+    .Call(R_sha512, buf)
+  }
 }
--- src/hashing.c
+++ src/hashing.c
@@ -8,9 +8,27 @@ SEXP R_sha256(SEXP buf){
   return res;
 }
 
+SEXP R_auth_sha256(SEXP buf, SEXP key){
+  if(LENGTH(key) != crypto_auth_hmacsha256_BYTES)
+    Rf_error("Invalid key, must be exactly %d bytes", crypto_auth_hmacsha256_BYTES);
+  SEXP res = allocVector(RAWSXP, crypto_hash_sha256_BYTES);
+  if(crypto_auth_hmacsha256(RAW(res), RAW(buf), LENGTH(buf), RAW(key)))
+    Rf_error("Failed to hash");
+  return res;
+}
+
 SEXP R_sha512(SEXP buf){
   SEXP res = allocVector(RAWSXP, crypto_hash_sha512_BYTES);
   if(crypto_hash_sha512(RAW(res), RAW(buf), LENGTH(buf)))
+    Rf_error("Failed to hash");
+  return res;
+}
+
+SEXP R_auth_sha512(SEXP buf, SEXP key){
+  if(LENGTH(key) != crypto_auth_hmacsha512_BYTES)
+    Rf_error("Invalid key, must be exactly %d bytes", crypto_auth_hmacsha512_BYTES);
+  SEXP res = allocVector(RAWSXP, crypto_hash_sha512_BYTES);
+  if(crypto_auth_hmacsha512(RAW(res), RAW(buf), LENGTH(buf), RAW(key)))
     Rf_error("Failed to hash");
   return res;
 }
