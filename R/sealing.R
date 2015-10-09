272bbd2c4792940d3fba3ced79d09f1af195347b
Jeroen Ooms <jeroenooms@gmail.com>
2015-10-09 19:10:54 +0200

    Docs

--- R/sealing.R
+++ R/sealing.R
@@ -1,45 +1,45 @@
-#' Simple Public-key Encryption (Sealed Box)
+#' Anonymous Public-key Encryption (Sealed Box)
 #'
 #' Create an encrypted message (sealed box) from a public key.
 #'
-#' Basic public key encryption allows for anonymously sending messages to a recipient
-#' given its public key. Only the recipient can decrypt these messages, using its private
-#' key.
+#' Simple public key encryption allows for sending anonymous encrypted messages to
+#' a recipient given its public key. Only the recipient can decrypt these messages,
+#' using its private key.
 #'
 #' While the recipient can verify the integrity of the message, it cannot verify the
 #' identity of the sender. For sending authenticated encrypted messages, use
 #' \link{secure_send} and \link{secure_recv}.
 #'
 #' @export
-#' @rdname sealing
-#' @name Sealed Box
+#' @rdname simple
+#' @name simple encryption
 #' @useDynLib sodium R_seal_box
 #' @references \url{http://doc.libsodium.org/public-key_cryptography/sealed_boxes.html}
-#' @param msg a message to be encrypted
+#' @param msg message to be encrypted
 #' @param key private key of the receiver
 #' @param pubkey public key of the receiver
-#' @param bin encrypted ciphertext returned by \code{encrypt}
+#' @param bin encrypted ciphertext
 #' @examples # Generate keypair
 #' key <- keygen()
 #' pub <- pubkey(key)
 #'
 #' # Encrypt message with pubkey
 #' msg <- serialize(iris, NULL)
-#' ciphertext <- encrypt(msg, pub)
+#' ciphertext <- simple_encrypt(msg, pub)
 #'
 #' # Decrypt message with private key
-#' out <- decrypt(ciphertext, key)
+#' out <- simple_decrypt(ciphertext, key)
 #' stopifnot(identical(out, msg))
-encrypt <- function(msg, pubkey){
+simple_encrypt <- function(msg, pubkey){
   stopifnot(is.raw(msg))
   stopifnot(is.raw(pubkey))
   .Call(R_seal_box, msg, pubkey)
 }
 
 #' @export
-#' @rdname sealing
+#' @rdname simple
 #' @useDynLib sodium R_seal_open
-decrypt <- function(bin, key){
+simple_decrypt <- function(bin, key){
   stopifnot(is.raw(bin))
   stopifnot(is.raw(key))
   .Call(R_seal_open, bin, key)
