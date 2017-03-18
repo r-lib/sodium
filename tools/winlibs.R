f235313e0a0fcb4ba6549027d66d5ba8d38c40b4
Jeroen Ooms <jeroenooms@gmail.com>
2017-03-18 18:19:23 +0100

    Update libsodium on Windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
-# Build against mingw-w64 build of libsodium 1.0.3
-if(!file.exists("../windows/sodium-1.0.3/include/sodium.h")){
+# Build against mingw-w64 build of libsodium
+if(!file.exists("../windows/sodium-1.0.12/include/sodium.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/sodium/archive/v1.0.3.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/sodium/archive/v1.0.12.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
