44b86a2613f84025fb17d75836d1596c35d5df09
Jeroen Ooms <jeroenooms@gmail.com>
2017-03-18 18:05:28 +0100

    Add symbol registration

--- src/init.c
+++ src/init.c
@@ -5,6 +5,8 @@
 void R_init_sodium(DllInfo *info) {
   if (sodium_init() < 0)
     Rf_error("Failed to initialize libsodium.");
+  R_registerRoutines(info, NULL, NULL, NULL, NULL);
+  R_useDynamicSymbols(info, TRUE);
 }
 
 void R_unload_sodium(DllInfo *info) {
