2b3c27a7f0fed51bca6c4795dc8eb5240fd22369
Jeroen Ooms <jeroenooms@gmail.com>
2020-11-20 14:10:20 +0100

    Allocate memory on the heap, fixes #19

--- src/helpers.c
+++ src/helpers.c
@@ -5,25 +5,29 @@
 SEXP R_sodium_bin2hex(SEXP bin){
   size_t bin_len = LENGTH(bin);
   size_t hex_len = bin_len * 2 + 1;
-  char hex[hex_len];
-  if(NULL == sodium_bin2hex(hex, hex_len, RAW(bin), bin_len))
+  char *hex = malloc(hex_len);
+  if(NULL == sodium_bin2hex(hex, hex_len, RAW(bin), bin_len)){
+    free(hex);
     Rf_error("Overflow error, failed to convert to hex");
-  SEXP res = PROTECT(allocVector(STRSXP, 1));
-  SET_STRING_ELT(res, 0, mkChar(hex));
-  UNPROTECT(1);
+  }
+  SEXP res = Rf_mkString(hex);
+  free(hex);
   return res;
 }
 
 SEXP R_sodium_hex2bin(SEXP hex, SEXP ignore){
   int hex_len = LENGTH(STRING_ELT(hex, 0));
   int max_len = hex_len / 2;
-  unsigned char bin[max_len];
+  unsigned char *bin = malloc(max_len);
   size_t bin_len;
   const char * hex_end;
-  if(sodium_hex2bin(bin, max_len, CHAR(STRING_ELT(hex, 0)), hex_len, CHAR(STRING_ELT(ignore, 0)), &bin_len, &hex_end))
+  if(sodium_hex2bin(bin, max_len, CHAR(STRING_ELT(hex, 0)), hex_len, CHAR(STRING_ELT(ignore, 0)), &bin_len, &hex_end)){
+    free(bin);
     Rf_error("Overflow error, failed to parse hex.");
+  }
   SEXP res = allocVector(RAWSXP, bin_len);
   memcpy(RAW(res), bin, bin_len);
+  free(bin);
   return res;
 }
 
